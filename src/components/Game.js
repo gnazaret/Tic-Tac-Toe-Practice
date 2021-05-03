a7173aa617f0a6105cb2c149f440427dabc87e5c
German <gnazaret@gmail.com>
2021-05-03 18:30:18 -0400

    game finished

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { calculateWinner} from '../function';
 import Board from './Board';
 
+const styles = {
+    width: '200px',
+    margin: '20px auto'
+}
 const Game = () => {
 
     const [board, setBoard] = useState(Array(9).fill(null));
@@ -9,7 +13,14 @@ const Game = () => {
 
     const winner = calculateWinner(board);
 
-    const handleClick = () => {
+    const handleClick = i => {
+        const board2 = [...board];
+        //if user click an occupied square or if game is won, return
+        if (winner || board2[i]) return;
+        // Put an X or an O in the clicked square
+        board2[i] = xIsNext ? 'X' : 'O';
+        setBoard(board2);
+        setXisNext(!xIsNext);
 
     }
     
@@ -19,11 +30,25 @@ const Game = () => {
     
     }
     
-    const renderMoves = () => {
+    const renderMoves = () => (
+        <button onClick={() => setBoard(Array(9).fill(null))}>
+                Start Game
+        </button>
+        )
         
-    }
+    
     return (
-       <Board squares={board} onClick={handleClick} />
+        <>
+         <Board squares={board} onClick={handleClick} />
+         <div style={styles}>
+             <p>
+                 {winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}
+                 {renderMoves()}
+             </p>
+
+         </div>
+
+       </>
     )
 }
 
